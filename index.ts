f6057d753577de8266b0dade8ffd19f8db5cec2a
border-radius <pharshmaster@gmail.com>
2023-05-19 16:05:00 +0300

    Demo

--- index.ts
+++ index.ts
@@ -4,9 +4,20 @@ import path from 'path';
 const PORT = process.env.PORT || 5000;
 const server = express();
 
+const ids: string[] = [];
+
 express()
   .use(express.static(path.join(__dirname, '../public')))
-  .set('views', path.join(__dirname, '../views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/c/:id', (req, res, next) => {
+    ids.push(req.params.id);
+    res.redirect('/c.html');
+  })
+  .get('/w/:id', (req, res, next) => {
+    const i = setInterval(() => {
+      if (ids.indexOf(req.params.id) > -1) {
+        clearInterval(i);
+        res.send('OK');
+      }
+    }, 300);
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
