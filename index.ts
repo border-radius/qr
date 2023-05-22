f88ed67bffa539b947e437bb702e38f9336b7dee
border-radius <pharshmaster@gmail.com>
2023-05-22 17:12:50 +0300

    Real recaptcha, timeout on redirect, redirect to youtube, qr code with query params and direct link

--- index.ts
+++ index.ts
@@ -2,22 +2,35 @@ import express from 'express';
 import path from 'path';
 
 const PORT = process.env.PORT || 5000;
-const server = express();
 
 const ids: string[] = [];
+const MAX_TIMEOUT = 10000;
+const REDIRECT = 'https://ak.psaltauw.net/4/5967693';
 
 express()
   .use(express.static(path.join(__dirname, '../public')))
   .get('/c/:id', (req, res, next) => {
     ids.push(req.params.id);
-    res.redirect('/c.html');
+    const qs = Object.keys(req.query).map(key => [key, req.query[key]].join('=')).join('&');
+    res.redirect(`${REDIRECT}?${qs}`);
   })
   .get('/w/:id', (req, res, next) => {
+    let sent = false;
+
     const i = setInterval(() => {
-      if (ids.indexOf(req.params.id) > -1) {
+      if (ids.indexOf(req.params.id) > -1 && !sent) {
+        sent = true;
         clearInterval(i);
         res.send('OK');
       }
     }, 300);
+
+    setTimeout(() => {
+      if (!sent) {
+        sent = true;
+        clearInterval(i);
+        res.send('TIMEOUT');
+      }
+    }, MAX_TIMEOUT);
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
